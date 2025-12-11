31c47b7933f317e521763486aa57ed07ce285717
Mayank Parab <mayankparab2006@gmail.com>
2025-12-11 17:28:48 +0530

    First Commit

---
+++ project/src/main/java/com/crud/project/model/Student.java
@@ -0,0 +1,47 @@
+package com.crud.project.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="student_data")
+public class Student {
+    @Id
+    private int id;
+    @Column(name="First_Name")
+    private String firstName;
+    @Column(name="Last_Name")
+    private String lastName;
+    @Column(name="CLass")
+    private String className;
+
+    public int getId() {return id;}
+
+    public void setId(int id) {this.id = id;}
+
+    public String getFirstName() {return firstName;}
+
+    public void setFirstName(String firstName) {this.firstName = firstName;}
+
+    public String getLastName() {return lastName;}
+
+    public void setLastName(String lastName) {this.lastName = lastName;}
+
+    public String getClassName() {return className;}
+
+    public void setClassName(String className) {this.className = className;}
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", className='" + className + '\'' +
+                '}';
+    }
+
+
+}
